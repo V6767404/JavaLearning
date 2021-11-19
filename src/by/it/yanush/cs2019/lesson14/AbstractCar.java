6ecaaeea2cf4b109b9b7987bd4cf975caf86f04e
V6767404 <32267531+V6767404@users.noreply.github.com>
2021-11-19 15:23:13 +0300

    commit

---
+++ src/by/it/yanush/cs2019/lesson14/AbstractCar.java
@@ -0,0 +1,30 @@
+package by.it.yanush.cs2019.lesson14;
+
+public class AbstractCar {
+    private int speed;
+
+    AbstractCar(int speed) {
+        this.speed = speed;
+    }
+
+    void fire() {
+        System.out.println(this + " включил зажигание");
+    }
+
+    void start() {
+        System.out.println(this + " поехал со скоростью " + speed + " км/ч");
+    }
+
+    void beep() {
+        System.out.println(this + " сигналит: Пи-бип!");
+    }
+
+    void stop() {
+        System.out.println(this + " остановился");
+    }
+
+    @Override
+    public String toString() {
+        return "Автомобиль";
+    }
+}
